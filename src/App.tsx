c70d30e798e0fea141a626d1ad635630ac8cd903
Kyle Bebeau <kyle@theinitgroup.com>
2023-03-06 10:01:47 -0500

    initial setup and proof display of globe package

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import GlobeRender from '@bebeau/globe';
+
+const userLocation = {
+  title: 'Detroit',
+  coordinates: {
+    lng: -70.641997,
+    lat: -33.46912,
+  },
+};
+
+const messages = [
+  {
+    "location": {
+      "state": "North Carolina",
+      "countryCode": "USA"
+    },
+    "message": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    "sender": "Susan"
+  },
+  {
+    "location": {
+      "state": "South Carolina",
+      "countryCode": "USA"
+    },
+    "message": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    "sender": "Walter"
+  },
+  {
+    "location": {
+      "state": "Pittsburgh",
+      "countryCode": "USA"
+    },
+    "message": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    "sender": "Henry"
+  }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobeRender
+      clientId="63bda1c87a9e114ce60a713e"
+      theme="child"
+      messages={messages}
+      currentUser="Corey"
+      currentLocation={userLocation}
+      exit={() => {}}
+    />
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GlobeRender from '@bebeau/globe';
+
+const userLocation = {
+  title: 'Detroit',
+  coordinates: {
+    lng: -70.641997,
+    lat: -33.46912,
+  },
+};
+
+const messages = [
+  {
+    "location": {
+      "state": "North Carolina",
+      "countryCode": "USA"
+    },
+    "message": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    "sender": "Susan"
+  },
+  {
+    "location": {
+      "state": "South Carolina",
+      "countryCode": "USA"
+    },
+    "message": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    "sender": "Walter"
+  },
+  {
+    "location": {
+      "state": "Pittsburgh",
+      "countryCode": "USA"
+    },
+    "message": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    "sender": "Henry"
+  }
+];
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <GlobeRender
+    clientId="63bda1c87a9e114ce60a713e"
+    theme="child"
+    messages={messages}
+    currentUser="Corey"
+    currentLocation={userLocation}
+    exit={() => {}}
+  />
 );
 
 // If you want to start measuring performance in your app, pass a function
