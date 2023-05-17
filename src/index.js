3b0442d5e1a8e7019271990eb48838568c79ca13
Kyle Bebeau <kyle@theinitgroup.com>
2023-05-17 11:32:44 -0400

    add expo webpack-config

---
+++ src/index.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import {View, StyleSheet, AppRegistry, Platform} from 'react-native';
+import GlobeRender from '@bebeau/globe';
+import './styles.css';
+
+const messages = [
+  {
+    "location": {
+      "state": "North Carolina",
+      "countryCode": "USA"
+    },
+    "message": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    "sender": "Susan"
+  },
+  {
+    "location": {
+      "state": "South Carolina",
+      "countryCode": "USA"
+    },
+    "message": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    "sender": "Walter"
+  },
+  {
+    "location": {
+      "state": "Pittsburgh",
+      "countryCode": "USA"
+    },
+    "message": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    "sender": "Henry"
+  }
+];
+
+const defaultProps = {
+  clientId: '63bda1c87a9e114ce60a713e',
+  // manualDataPoints: dataPoints,
+  messages: messages,
+  theme: 'child',
+  type: 'send',
+  currentUser: 'Corey',
+  currentLocation: {
+    id: '0',
+    title: 'Detroit',
+    coordinates: {
+      lng: -83,
+      lat: 42,
+    },
+  },
+  exit: () => {},
+};
+
+function App() {
+  return (
+    <View style={styles.container}>
+      <GlobeRender {...defaultProps} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: '100%',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+AppRegistry.registerComponent('webglglobe', () => App);
+
+if (Platform.OS === 'web') {
+  AppRegistry.runApplication('webglglobe', {
+    rootTag: document.getElementById('root'),
+    initialProps: defaultProps,
+  });
+}
